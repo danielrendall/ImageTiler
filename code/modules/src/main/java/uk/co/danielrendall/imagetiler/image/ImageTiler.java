8b0cca0c4715341858ef33d5cd4bc4a4874c67d1
Daniel Rendall <drendall@gmail.com>
2010-03-06 17:57:20 +0000

    Take input file and output file from args (note, only seems to like JPGs at the moment)

--- code/modules/src/main/java/uk/co/danielrendall/imagetiler/image/ImageTiler.java
+++ code/modules/src/main/java/uk/co/danielrendall/imagetiler/image/ImageTiler.java
@@ -1,5 +1,9 @@
 package uk.co.danielrendall.imagetiler.image;
 
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.CmdLineException;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -14,41 +18,73 @@ import java.io.IOException;
  */
 public class ImageTiler {
 
-    public final static String INPUT_FILENAME = "C:\\heart.tif";
-    public final static String OUTPUT_FILENAME = "C:\\heartout.tif";
+    @Option(name = "-i", usage = "the input file", required = true)
+    private File inputFile;
+
+    @Option(name = "-o", usage = "the output file", required = true)
+    private File outputFile;
 
     public static void main(String[] args) {
+        new ImageTiler().doMain(args);
+    }
+
+    public void doMain(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+
+        parser.setUsageWidth(80);
+
+        try {
+            // parse the arguments.
+            parser.parseArgument(args);
+            process();
+
+        } catch (CmdLineException e) {
+            // if there's a problem in the command line,
+            // you'll get this exception. this will report
+            // an error message.
+            System.err.println(e.getMessage());
+            System.err.println("java ImageTiler [options...] arguments...");
+            // print the list of available options
+            parser.printUsage(System.err);
+            System.err.println();
+        }
+    }
+
+    private void process() {
         ImageTile imageTile = new GemImageTile();
-        imageTile.initialize(args);
+//        imageTile.initialize(args);
         final int tileWidth = imageTile.getWidth();
         final int tileHeight = imageTile.getHeight();
         try {
-            File inFile = new File(INPUT_FILENAME);
-            BufferedImage bi = ImageIO.read(inFile);
+            BufferedImage bi = ImageIO.read(inputFile);
 
-            BufferedImage biOut = new BufferedImage(bi.getWidth() * tileWidth, bi.getHeight() * tileHeight, BufferedImage.TYPE_INT_RGB);
+            if (bi != null) {
 
-            Graphics2D g =biOut.createGraphics();
-            g.setColor(Color.WHITE);
-            g.fillRect(0,0,biOut.getWidth(), biOut.getHeight());
+                BufferedImage biOut = new BufferedImage(bi.getWidth() * tileWidth, bi.getHeight() * tileHeight, BufferedImage.TYPE_INT_RGB);
 
-            WritableRaster wr = biOut.getRaster();
+                Graphics2D g =biOut.createGraphics();
+                g.setColor(Color.WHITE);
+                g.fillRect(0,0,biOut.getWidth(), biOut.getHeight());
+
+                WritableRaster wr = biOut.getRaster();
 
 
-            for (int y=0; y < bi.getHeight(); y++) {
-                for (int x=0; x < bi.getWidth(); x++) {
-                    int pixel[] = new int[4];
-                    bi.getRaster().getPixel(x,y,pixel);
-                    Color color = new Color(pixel[0], pixel[1], pixel[2]);
-                    int xTile = x * tileWidth;
-                    int yTile = y * tileHeight;
-                    imageTile.getTile(biOut.getSubimage(xTile, yTile, tileWidth, tileHeight), color);
+                for (int y=0; y < bi.getHeight(); y++) {
+                    for (int x=0; x < bi.getWidth(); x++) {
+                        int pixel[] = new int[4];
+                        bi.getRaster().getPixel(x,y,pixel);
+                        Color color = new Color(pixel[0], pixel[1], pixel[2]);
+                        int xTile = x * tileWidth;
+                        int yTile = y * tileHeight;
+                        imageTile.getTile(biOut.getSubimage(xTile, yTile, tileWidth, tileHeight), color);
 
+                    }
                 }
+                biOut.setData(wr);
+                ImageIO.write(biOut, "jpg", outputFile);
+            } else {
+                System.out.println("Couldn't read image");
             }
-            File outFile = new File(OUTPUT_FILENAME);
-            biOut.setData(wr);
-            ImageIO.write(biOut, "tif", outFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
