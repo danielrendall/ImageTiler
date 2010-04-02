294abf99f9187a0d98dd2d689b24949a7b4f95af
Daniel Rendall <drendall@gmail.com>
2010-04-02 13:34:21 +0000

    Ignore output svg in resources directory, add a new bmp and a useful command-line script

---
+++ resources/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "X$1" == "X" ]
+then
+echo "No input file specified"
+exit 1
+fi
+
+if [ "X$2" == "X" ]
+then
+echo "No tile type specified"
+exit 1
+fi
+
+if [ "X$3" == "X" ]
+then
+echo "No scan format specified"
+exit 1
+fi
+
+
+java -jar ../code/modules/target/ImageTiler.jar  -f svg  -i $1 -t $2 -s $3 -o $1.svg -c "$4"
