cb012f4ae7006fd3419fe8ebc6aeeed042fc7877
Daniel Rendall <drendall@gmail.com>
2011-09-12 23:43:47 +0100

    Set the version from the POM files in a slightly hacky way

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-VERSION=1.0
+CORE_VERSION=`grep "<version>" code/core/pom.xml | head -n 1 | cut -d '>' -f 2 | cut -d '<' -f 1`
+TILES_VERSION=`grep "<version>" code/tiles/pom.xml | head -n 1 | cut -d '>' -f 2 | cut -d '<' -f 1`
 
-if [ -f "code/core/target/imagetiler-"$VERSION"-jar-with-dependencies.jar" ]
+if [ -f "code/core/target/imagetiler-"$CORE_VERSION"-jar-with-dependencies.jar" ]
 then
-	java -cp  "code/tiles/target/imagetiler-tiles-1.0.jar:code/core/target/imagetiler-"$VERSION"-jar-with-dependencies.jar" uk.co.danielrendall.imagetiler.ImageTilerApplication
+	java -cp  "code/tiles/target/imagetiler-tiles-"$TILES_VERSION".jar:code/core/target/imagetiler-"$CORE_VERSION"-jar-with-dependencies.jar" uk.co.danielrendall.imagetiler.ImageTilerApplication
 else
 	echo ImageTiler doesn''t appear to be built - maybe you need to run build.sh
 fi
