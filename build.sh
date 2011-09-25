471ee23e4afc3ed7621190b66483f8a6778d8cc4
Daniel Rendall <drendall@gmail.com>
2011-09-25 14:11:35 +0100

    Don't specify version of compiler plugin, also don't run Maven in offline mode when assembling project

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pushd code/core && mvn clean && mvn install && mvn -o assembly:single && popd
+pushd code/core && mvn clean && mvn install && mvn assembly:single && popd
 pushd code/tiles && mvn clean && mvn install  && popd
 
